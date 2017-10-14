a651355fe83e37197cde19d773c125971ffb1981
Allen Gong <alm.gong@gmail.com>
2017-10-14 14:21:27 -0700

    Create Chunker to chunk arbitrary blobs

--- src/lib/chunker.js
+++ src/lib/chunker.js
@@ -1,5 +1,78 @@
-function a () {
-	console.log('chunked');
-}
+/**
+ * Module that allows chunking of an arbitrary blob. Chunkers expose operation(s)
+ * to iteratively return chunks (e.g. similar to a generator/iterator).
+ */
+export default class Chunker {
+	/**
+	 * Constructs a Chunker instance
+	 * 
+	 * @param  { Blob } 					blob    instance of Blob (or something that quacks like one)
+	 * @param  { Object } 				options optional overrides that affect chunking operations
+	 * @return { Chunker Object }         a Chunker instance
+	 */
+	constructor(blob, options) {
+		if(!blob || typeof blob.slice != 'function' || typeof blob.size != 'function') {
+			throw new Error('Chunker requires a Blob like object');
+		} else if (options && !this._validateOptions(options)) {
+			throw new Error('Invalid options');
+		}
 
-export default a;
+		let chunkOptions = {
+			chunkSize: 1000000,	// in bytes, defaults to 1MB
+		};
+
+		Object.assign(chunkOptions, options || {});
+
+		this._blob = blob;
+		this._options = chunkOptions;
+		this._chunkOffset = 0;
+	}
+
+	/**
+	 * Returns the total number of chunks needed to completely split the specfied blob.
+	 * @return { Integer } integer number of chunks
+	 */
+	numChunksNeeded() {
+		return Math.ceil(this._blob.size() / this._options.chunkSize);
+	}
+
+	/**
+	 * Returns the next chunk. Chunks are returned in the order they exist within the
+	 * blob. This should be the main operation utilized to eventually retrieve all blob chunks.
+	 * @return { Blob } a blob chunk of at most chunkSize bytes, or null if there are no more chunks
+	 */
+	next() {
+		return this.chunkAt(this._chunkOffset++);
+	}
+
+	/**
+	 * Returns a specific chunk based on its index. Similar to String methods such as
+	 * charAt(), if the chunk number is known, this method can be used to directly access
+	 * a specific chunk (rather than having to go through next() all over again).
+	 * @param  { Integer } index	the chunk number (zero based)
+	 * @return { Blob }						a blob chunk of at most chunkSize bytes, or null if there are no bytes at the index							     
+	 */
+	chunkAt(index) {
+		let nextChunkStart = index * this._options.chunkSize;
+		let chunk = this._blob.slice(nextChunkStart, nextChunkStart + this._options.chunkSize);
+
+		return (chunk.size() > 0) ? chunk : null
+	}
+
+	/**
+	 * Internally used to validate options passed into the constructor.
+	 * @param  { Object } options 	options passed into the constructor
+	 * @return { Boolean }         	whether the options are valid
+	 */
+	_validateOptions(options) {
+		let isNotDefined = (obj) => {
+			return obj === undefined;
+		};
+
+		if (isNotDefined(options.chunkSize) || typeof options.chunkSize == "number" && options.chunkSize > 0) {
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ test/lib/chunker_test.js
@@ -0,0 +1,167 @@
+import assert from 'assert';
+import simple from 'simple-mock';
+import Chunker from '../../src/lib/chunker';
+
+/**
+ * Unit tests for lib/requester.js. NO arrow functions in test setup description
+ * due to `this` binding issues.
+**/
+
+describe('Chunker', function() {
+	afterEach(function() {
+    simple.restore();	// restores state of objects
+  });
+
+	describe('#constructor', function() {
+		it('should be able to construct a default Chunker without options', function() {
+			// hooray for ducktyping
+			let blob = simple.stub();
+			simple.mock(blob, 'size').returnWith(null);
+			simple.mock(blob, 'slice').returnWith(null);
+
+			new Chunker(blob);
+		});
+
+		it('should be able to construct a Chunker with options', function() {
+			let blob = simple.stub();
+			simple.mock(blob, 'size').returnWith(null);
+			simple.mock(blob, 'slice').returnWith(null);
+
+			new Chunker(blob, {});
+
+			new Chunker(blob, { optionWeDontCareAbout: 1 });
+			new Chunker(blob, { chunkSize: 1 });
+		});
+
+		it('throws Error if constructing a Chunker without blob', function() {
+			assert.throws(
+				() => {
+					new Chunker();
+				},
+				/Chunker requires a Blob like object/
+			);
+		});
+
+		it('throws Error if constructing a Chunker with invalid chunkSize option', function() {
+			let blob = simple.stub();
+			simple.mock(blob, 'size').returnWith(null);
+			simple.mock(blob, 'slice').returnWith(null);
+
+			assert.throws(
+				() => {
+					new Chunker(blob, { chunkSize: -1 });
+				},
+				/Invalid options/
+			);
+
+			assert.throws(
+				() => {
+					new Chunker(blob, { chunkSize: 0 });
+				},
+				/Invalid options/
+			);
+
+			assert.throws(
+				() => {
+					new Chunker(blob, { chunkSize: null });
+				},
+				/Invalid options/
+			);
+		});
+	});
+
+	describe('#numChunksNeeded', function() {
+		it('should return the correct number of chunks needed to completely chunk the blob, default chunkSize', function() {
+			let blob = simple.stub();
+			simple.mock(blob, 'size').returnWith(10000000);
+			simple.mock(blob, 'slice').returnWith(null);
+
+			assert.equal(new Chunker(blob).numChunksNeeded(), 10);
+		});
+
+		it('should return the correct number of chunks needed to completely chunk the blob, specified chunkSize', function() {
+			let blob = simple.stub();
+			simple.mock(blob, 'size').returnWith(27);
+			simple.mock(blob, 'slice').returnWith(null);
+
+			assert.equal(new Chunker(blob, { chunkSize: 5 }).numChunksNeeded(), 6);
+		});
+	});
+
+	describe('#next', function() {
+		it('should return all chunks by iteratively calling next', function() {
+			let blobData = "abcdefg";
+			let blob = simple.stub();
+			simple.mock(blob, 'size').callFn(() => { return blobData.length; });
+			simple.mock(blob, 'slice').callFn((start, end) => { 
+				let chunkedBlob = simple.stub();
+				let chunkedData = blobData.slice(start, end);
+
+				simple.mock(chunkedBlob, 'size').callFn(() => { return chunkedData.length; });
+				simple.mock(chunkedBlob, 'data').returnWith(chunkedData);	// not a real Blob method, for testing only
+
+				return chunkedBlob;
+			});
+
+			let expectedIterations = 4;
+			let expectedBlobDataInSequence = ['ab', 'cd', 'ef', 'g'];
+
+			let chunker = new Chunker(blob, { chunkSize: 2 });
+			let chunk;
+			let iterations = 0;
+
+			while(chunk = chunker.next()) {
+				assert.equal(chunk.data(), expectedBlobDataInSequence[iterations++]);
+			}
+
+			assert.equal(iterations, expectedIterations);
+		});
+	});
+
+	describe('#chunkAt', function() {
+		it('should return randomly accessed chunks at a certain index', function() {
+			let blobData = "abcdefg";
+			let blob = simple.stub();
+			simple.mock(blob, 'size').callFn(() => { return blobData.length; });
+			simple.mock(blob, 'slice').callFn((start, end) => { 
+				let chunkedBlob = simple.stub();
+				let chunkedData = blobData.slice(start, end);
+
+				simple.mock(chunkedBlob, 'size').callFn(() => { return chunkedData.length; });
+				simple.mock(chunkedBlob, 'data').returnWith(chunkedData);	// not a real Blob method, for testing only
+
+				return chunkedBlob;
+			});
+
+			let expectedIterations = 4;
+			let expectedBlobDataInChunkSequence = ['ab', 'cd', 'ef', 'g'];
+			let chunker = new Chunker(blob, { chunkSize: 2 });
+			let unorderedAccessSequence = [2, 0, 1, 3];
+
+			for (let index of unorderedAccessSequence) {
+				let chunk = chunker.chunkAt(index);
+				assert.equal(chunk.data(), expectedBlobDataInChunkSequence[index]);
+			}
+		});
+
+		it('should return null if chunk number is out of range', function() {
+			let blobData = "abcdefg";
+			let blob = simple.stub();
+			simple.mock(blob, 'size').callFn(() => { return blobData.length; });
+			simple.mock(blob, 'slice').callFn((start, end) => { 
+				let chunkedBlob = simple.stub();
+				let chunkedData = blobData.slice(start, end);
+
+				simple.mock(chunkedBlob, 'size').callFn(() => { return chunkedData.length; });
+				simple.mock(chunkedBlob, 'data').returnWith(chunkedData);	// not a real Blob method, for testing only
+
+				return chunkedBlob;
+			});
+
+			let chunker = new Chunker(blob, { chunkSize: 2 });
+			
+			assert.equal(chunker.chunkAt(-1), null);
+			assert.equal(chunker.chunkAt(5), null);
+		});
+	});
+});
