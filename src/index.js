0e818d8f06fd26d4a0a477994bf34fbca936fb0c
Allen Gong <alm.gong@gmail.com>
2017-10-21 11:07:12 -0700

    Change index.js to be a stub file
    ChunkManager is pretty much the interface we want to expose anyways

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import ChunkManager from './lib/chunk_manager';
 
 /**
- * Service object for uploading arbitrary blobs to a specified endpoint.
- *
- * TODO: documentation on usage.
-**/
-class ChunkedClientUploader {
-	constructor(options) {
-		this._uploadIdEndpoint = options.uploadIdEndpoint;
-		this._uploadEndpoint = options.uploadEndpoint;
-	}
-
-	upload(Blob) {
-		
-	}
-
-	/** 
-	 * validates arguments passed in constructor
-	**/
-	_validateInitialization() {
-		if (!this._uploadIdEndpoint) {
-			throw new Error('A valid endpoint to retrieve an upload identifier is required');
-		} else if (!this._uploadEndpoint) {
-			throw new Error('A valid endpoint to retrieve an upload identifier is required');
-		}
-	}
-}
-
-export default A;
+ * Stub
+ */
+export default ChunkManager;
